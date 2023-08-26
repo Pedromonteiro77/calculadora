79338c64e356cd75c9bcf70340a9d816dd5d78a7
Pedromonteiro77 <107282276+Pedromonteiro77@users.noreply.github.com>
2023-08-26 18:57:30 -0300

    update 0.4

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <memory>
 
 int main() { 
     std::string nome;
@@ -8,6 +7,7 @@ int main() {
     double n2;
     char opera;
     char opc;
+    bool verifi;
 
     auto soma = [](double & a, double & b) -> double {
         return a + b;
@@ -24,19 +24,45 @@ int main() {
     auto divide = [](double & a, double & b) -> double {
         return a / b;
     };
-     
-    std::cout << "Oi Bem vindo me diga seu nome: ";
-    std::cin >> nome;
+
+    while(nome.empty()) {
+        while(nome.empty()) {
+            std::cout << "Oi Bem vindo me diga seu nome: ";
+            std::getline(std::cin, nome);
+            if(nome.empty()) {
+                std::cerr << "ERRO, Digite seu nome ou como gostaria de ser chamado!" << std::endl;
+                std::cout << "Pressione Enter para continuar...";
+                std::cin.get(); 
+                std::system("cls");
+            }
+        }
 
         while(true) {
+            std::cout << "Seu nome e " << nome << " esta correto? sim(s), nao(n): ";
+            std::cin >> opc;
+            std::cin.ignore();
+
+            if((opc == 's') || (opc == 'S')) {
+                std::system("cls");
+                break;
+            } 
+            else {
+                nome.clear();
+                std::system("cls");
+                break;
+            }
+        }
+    }
+
+    while(true) {
         std::cout << "Oi " << nome << " Me diga o que voce gostaria de calcular: soma(+), subtracao(-), multiplicacao(*), divisao(/): ";
         std::cin >> opera;
 
         if((opera != '+') && (opera != '-') && (opera != '*') && (opera != '/')) {
             std::cerr << "INVALIDO. Por favor digite um dos caracteres: (+), (-), (*), (/). Vamos tentar novamente" 
             << std::endl;
-            system("Pause");
-            system("cls");
+            std::system("Pause");
+            std::system("cls");
             continue;
         }
     
@@ -57,8 +83,8 @@ int main() {
         else if(opera == '/') {
             if((n2 == 0)) {
                 std::cerr << "Desculpe nao e possivel dividir por zero. Vamos tentar novamente" << std::endl;
-                system("pause");
-                system("cls");
+                std::system("pause");
+                std::system("cls");
                 continue;
             }
             else {
@@ -70,15 +96,14 @@ int main() {
         std::cin >> opc;
 
         if((opc == 's') || (opc == 'S')) {
-            system("cls");
+            std::system("cls");
             continue;
         }   
         else {
-            system("cls");
+            std::system("cls");
             std::cout << "Ok " << nome << " obrigado por usar meu programa :)" << "\n";
             break;
         }
-
     }
 
     return 0;
